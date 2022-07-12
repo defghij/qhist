8ef68606153e8ca79e7582ec894d77df2172f126
Chuck <norrisc@protonmail.com>
2022-07-11 20:38:34 -0400

    Minor changes to main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
 use std::path::PathBuf;
 use std::fs::File;
 use structopt::StructOpt;
@@ -7,39 +7,49 @@ use hdrhistogram::Histogram;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "qhist", about = "description")]
 struct Opt {
-    /// Input File
+    /// Input file to read
     #[structopt(short,long)]
     input: PathBuf,
 
-    /// Column
+    /// Column of input data which should be operated on.
     #[structopt(short, long)]
     column: usize,
 
-    /// Lower Percentile
+    /// Lower bound on displayed percentile
     #[structopt(short, long, default_value="0")]
     lower: f64,
 
-    /// upper Percentile
+    /// Upper bound on displayed percentile
     #[structopt(short, long, default_value="100")]
     upper: f64,
 
-    /// Max output
+    /// Maximum number of percentile lines to display
     #[structopt(short, long, default_value="100")]
     max_lines: usize,
+
+    /// Resolution of percentile display.
+    #[structopt(short, long, default_value="1")]
+    resolution: u64
+
 }
 
 fn main() {
-    
+   
+    // Set options
     let opt: Opt = Opt::from_args();
     let column: usize = opt.column;
     let f_name: PathBuf = opt.input;
     let max_lines: usize = opt.max_lines;
-    let upper_perc: f64= opt.upper;
+    let upper_perc: f64  = opt.upper;
     let lower_perc: f64 = opt.lower;
+    let reso: u64 = opt.resolution;
+
     if lower_perc > upper_perc {
         panic!("Lower percentile bound is greater than upper percentile bound");
     }
 
+
+    // Read in data
     let file:File = File::open(f_name).expect("file not found");
 
     let lines: Vec<u64> = io::BufReader::new(file)
@@ -53,24 +63,33 @@ fn main() {
             l[column].to_owned().parse::<u64>().expect("Value was not parsable to an integer")
     }).collect();
 
+
+    // Populate histogram
     let mut hist = Histogram::<u64>::new(3).expect("Unable to create histogram");
 
     for val in lines.iter() {
         hist.record(*val).expect("Value added to histogram is out of range");
     }
-    let count: u64 = hist.len();
-    let stddev = hist.stdev();
-    let mean = hist.mean();
 
-    println!("Samples: {0: >7}", count);
+
+    // Print out the information
+    println!("Samples: {0: >7}", hist.len());
     println!("Max:  {0: >10.2}", hist.highest_equivalent(hist.value_at_percentile(100.)));
     println!("Min:  {0: >10.2}", hist.lowest_equivalent(hist.value_at_percentile(0.)));
-    println!("Mean: {0: >10.2}", mean);
-    println!("SD:   {0: >10.2}", stddev);
+    println!("Mean: {0: >10.2}", hist.mean());
+    println!("SD:   {0: >10.2}", hist.stdev());
+    if (hist.mean() + 3. * hist.stdev()) <= hist.max() as f64{
+        println!("Outlier(s) >= {0: >10.2}", hist.mean() + 3. * hist.stdev());
+    }
+    if (hist.mean() - 3. * hist.stdev()) >= hist.min() as f64{
+        println!("Outlier(s) <= {0: >10.2}", hist.mean() + 3. * hist.stdev());
+    }
+
     println!("Percentile  count      value");
 
     let mut out: Vec<String> = Vec::new();
-    for v in hist.iter_recorded() {
+    for v in hist.iter_linear(reso) {
+
         if lower_perc < v.percentile() &&
            v.percentile() <= upper_perc && 
            v.count_at_value() != 0 {
