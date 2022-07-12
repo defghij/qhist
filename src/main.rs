5773c0c3e003ac85fa86e96860595149bf3c99ca
defghij <42917015+defghij@users.noreply.github.com>
2022-07-11 23:53:35 -0400

    Use stdio handle instead of println macro

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,11 @@ struct Opt {
 
     /// Lower bound on displayed percentile
     #[structopt(short, long, default_value="0")]
-    lower: f64,
+    lower: u64,
 
     /// Upper bound on displayed percentile
     #[structopt(short, long, default_value="100")]
-    upper: f64,
+    upper: u64,
 
     /// Maximum number of percentile lines to display
     #[structopt(short, long, default_value="100")]
@@ -33,34 +33,28 @@ struct Opt {
 
 }
 
-fn main() {
-   
-    // Set options
-    let opt: Opt = Opt::from_args();
-    let column: usize = opt.column;
-    let f_name: PathBuf = opt.input;
-    let max_lines: usize = opt.max_lines;
-    let upper_perc: f64  = opt.upper;
-    let lower_perc: f64 = opt.lower;
-    let reso: u64 = opt.resolution;
+fn main() -> Result<(), std::io::Error> {
 
-    if lower_perc > upper_perc {
+    // Set options
+    let args: Opt = Opt::from_args();
+
+    if args.lower > args.upper {
         panic!("Lower percentile bound is greater than upper percentile bound");
     }
 
 
     // Read in data
-    let file:File = File::open(f_name).expect("file not found");
+    let file:File = File::open(args.input).expect("file not found");
 
     let lines: Vec<u64> = io::BufReader::new(file)
         .lines()
         .map(|line| {
             let l = line.unwrap();
             let l: Vec<&str> = l.split(" ").collect();
-            if l.len() <= column {
+            if l.len() <= args.column {
                 panic!("Error! Given column does not exist in data for line:\n---\n{0}\n----", l.clone()[0]);
             }
-            l[column].to_owned().parse::<u64>().expect("Value was not parsable to an integer")
+            l[args.column].to_owned().parse::<u64>().expect("Value was not parsable to an integer")
     }).collect();
 
 
@@ -73,25 +67,60 @@ fn main() {
 
 
     // Print out the information
-    println!("Samples: {0: >7}", hist.len());
-    println!("Max:  {0: >10.2}", hist.highest_equivalent(hist.value_at_percentile(100.)));
-    println!("Min:  {0: >10.2}", hist.lowest_equivalent(hist.value_at_percentile(0.)));
-    println!("Mean: {0: >10.2}", hist.mean());
-    println!("SD:   {0: >10.2}", hist.stdev());
+    let stdout = std::io::stdout();
+    let mut stdout = stdout.lock();
+    write_header(&hist, &mut stdout)?;
+
+
+    let percentiles = construct_percentiles(&hist, 
+        args.resolution,
+        args.upper, 
+        args.lower);
+
+    write_percentiles(&percentiles, args.max_lines, &mut stdout)?;
+    
+    Ok(())
+}
+
+fn write_header<W: Write>(hist: &Histogram<u64>, writer: &mut W)
+-> Result<(), std::io::Error> {
+    writer.write_all(format!(
+            "Samples: {0: >7}\n\
+            Max:  {1: >10.2}\n\
+            Min:  {2: >10.2}\n\
+            Mean: {3: >10.2}\n\
+            SD:   {4: >10.2}\n", 
+            hist.len(),
+            hist.highest_equivalent(hist.value_at_percentile(100.)), 
+            hist.lowest_equivalent(hist.value_at_percentile(0.)),
+            hist.mean(), 
+            hist.stdev()).as_ref()
+    )?;
+
+
     if (hist.mean() + 3. * hist.stdev()) <= hist.max() as f64{
-        println!("Outlier(s) >= {0: >10.2}", hist.mean() + 3. * hist.stdev());
+        writer.write_all(format!("Outlier(s) >= {0: >10.2}",
+            hist.mean() + 3. * hist.stdev()
+        ).as_ref())?;
     }
     if (hist.mean() - 3. * hist.stdev()) >= hist.min() as f64{
-        println!("Outlier(s) <= {0: >10.2}", hist.mean() + 3. * hist.stdev());
+        writer.write_all(format!("Outlier(s) <= {0: >10.2}\n",
+            hist.mean() + 3. * hist.stdev()).as_ref()
+        )?;
     }
+    Ok(())
+}
 
-    println!("Percentile  count      value");
+fn construct_percentiles(hist: &Histogram<u64>,
+               resolution: u64,
+               upper_bound: u64,
+               lower_bound: u64) 
+-> Vec<String> {
 
     let mut out: Vec<String> = Vec::new();
-    for v in hist.iter_linear(reso) {
-
-        if lower_perc < v.percentile() &&
-           v.percentile() <= upper_perc && 
+    for v in hist.iter_linear(resolution) {
+        if lower_bound as f64 <= v.percentile() &&
+           v.percentile() <= upper_bound as f64 && 
            v.count_at_value() != 0 {
             out.push(format!("{: >6.2} {: >10} {: >10}",
                 v.percentile(),v.count_at_value(), v.value_iterated_to()));
@@ -99,9 +128,18 @@ fn main() {
     } 
 
     out.reverse();
-    let line_count = if out.len() < max_lines { out.len() } else { max_lines };
-    for l in out[0..line_count].iter() {
-        println!("{}", l);
-    }
+    out
+}
 
+fn write_percentiles<W: Write>(percentiles: &Vec<String>, 
+                               max_lines: usize,
+                               writer: &mut W)
+-> Result<(), std::io::Error> {
+    writer.write_all(format!("Percentile  count      value\n").as_ref())?;
+    let line_count = if percentiles.len() < max_lines { percentiles.len() } else { max_lines };
+    for l in percentiles[0..line_count].iter() {
+        writer.write_all(format!("{}\n", l).as_ref())?;
+    }   
+
+    Ok(())
 }
