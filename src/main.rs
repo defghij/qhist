a01cb28c502e9b4af2d77fd387b2ef6746062af2
Chuck <norrisc@protonmail.com>
2022-07-14 16:58:41 -0400

    Added barchart

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,23 @@ use hdrhistogram::Histogram;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "qhist", about = "Simple historgraphic information")]
 struct Opt {
-    /// Input file to read
+    /// Path to file
     #[structopt(short, long, parse(from_os_str))]
     input: Option<PathBuf>,
 
-    /// Column of input data which should be operated on.
+    /// The space delimited column to read data from (zero indexed)
+    /// 
+    /// The column of data may contain spaces any number of spaces 
+    /// on either side of the u64 value. Default value is zero to allow
+    /// for easy piping of data from STDIN.
     #[structopt(short, long, default_value="0")]
     column: usize,
 
-    /// Lower bound on displayed percentile
+    /// Lowest percentile to display
     #[structopt(short, long, default_value="0")]
     lower: u64,
 
-    /// Upper bound on displayed percentile
+    /// Highest percentile to display
     #[structopt(short, long, default_value="100")]
     upper: u64,
 
@@ -28,29 +32,62 @@ struct Opt {
     #[structopt(short, long, default_value="100")]
     max_lines: usize,
 
-    /// Resolution of percentile display.
+    /// Bucket size for percentile display.
+    /// 
+    /// When this option is supplied the iteration method to generate
+    /// buckets is linear stepping at `--resolution` values. This stepping
+    /// can, and will, lead to larger bucket counts which can invert 
+    /// the scaling for the simple barchart. See `--bar-length` for more
+    /// information.
     #[structopt(short, long)]
     resolution: Option<u64>,
 
-    /// Do not print info simple info block
+    /// Do not print simple info block
     #[structopt(long)]
     no_info: bool,
 
-    /// Do not print percentiles
+    /// Do not print percentiles. Implies `--no-bars`.
     #[structopt(long)]
     no_percentiles: bool,
+
+    /// Do not print simple bar chart
+    #[structopt(long)]
+    no_bars: bool,
+
+    /// Max bar length
+    /// 
+    /// This option controls the number of tick marks in the longest
+    /// bar of the chart. That is, the count for each value is normalized
+    /// to [0,1) and multipled by `--bar-length`. When `--resolution` is
+    /// given the bucket size is altered and no longer known at histogram
+    /// creation. This may result in inverse scaling of the bar chart. When
+    /// this occurs use smaller values for `--bar-length`
+    #[structopt(short, long, default_value="100")]
+    bar_length: f64,
 }
 
 
 fn main() -> Result<(), std::io::Error> {
 
-    // Set options
+    // Get options
     let args: Opt = Opt::from_args();
 
+    // Some argument validation
     if args.lower > args.upper {
         panic!("Lower percentile bound is greater than upper percentile bound");
     }
 
+    // Simpe barchart related argument dependencies.
+    let no_bars = match args.no_percentiles {
+        true => true,
+        false => args.no_bars
+    };
+    let bar_length: f64 = match no_bars {
+        true => 0.,
+        false => args.bar_length
+    };
+        
+        
     // Read in data
     let lines: Vec<u64>;
     if args.input == None {
@@ -65,10 +102,14 @@ fn main() -> Result<(), std::io::Error> {
     }
 
     // Populate histogram
+    let mut max_count: u64 = u64::MIN;
+    let mut min_count: u64 = u64::MAX;
     let mut hist = Histogram::<u64>::new(3).expect("Unable to create histogram");
 
     for val in lines.iter() {
         hist.record(*val).expect("Value added to histogram is out of range");
+        if hist.count_at(*val) > max_count { max_count = hist.count_at(*val); }
+        else if hist.count_at(*val) < min_count { min_count = hist.count_at(*val); }
     }
 
 
@@ -81,28 +122,32 @@ fn main() -> Result<(), std::io::Error> {
     }
 
     if !args.no_percentiles {
-        let scaling: f64 = 1. / 9.;
         let percentiles = match args.resolution {
             Some(resolution) => {
                 construct_percentiles(&mut hist.iter_linear(resolution), 
                                        args.upper, 
                                        args.lower,
-                                        scaling)
+                                        max_count,
+                                        min_count,
+                                        bar_length)
             }
             None => {
                 construct_percentiles(&mut hist.iter_recorded(), 
                                        args.upper, 
                                        args.lower,
-                                        scaling)
+                                        max_count,
+                                        min_count,
+                                        bar_length)
             }
         };
 
-        write_percentiles_to(&mut stdout, &percentiles, args.max_lines)?;
+        write_percentiles_to(&mut stdout, &percentiles, args.max_lines, args.no_info)?;
     }
     
     Ok(())
 }
 
+/// Returns a vector containing the data pointed to by column and reader.
 fn read_data_from<R: BufRead>(reader: R, column: usize) -> Vec<u64> {
     let lines: Vec<u64> = reader
         .lines()
@@ -118,6 +163,8 @@ fn read_data_from<R: BufRead>(reader: R, column: usize) -> Vec<u64> {
     lines
 }
 
+
+/// Prints simple histographic information to STDOUT
 fn write_info_to<W: Write>(writer: &mut W, hist: &Histogram<u64>)
 -> Result<(), std::io::Error> {
     writer.write_all(format!(
@@ -146,10 +193,14 @@ fn write_info_to<W: Write>(writer: &mut W, hist: &Histogram<u64>)
     Ok(())
 }
 
+/// Constructs the percentile, and optionally the barchart, strings line by line
+/// from a `HistogramIterator` which is returned as a vector of `String`s.
 fn construct_percentiles<I: PickyIterator<u64>>(hist: &mut HistogramIterator<u64,I>,
                          upper_bound: u64,
                          lower_bound: u64,
-                         scaling: f64) 
+                         max: u64,
+                         min: u64,
+                         bar_length: f64)
 -> Vec<String> {
 
     let mut out: Vec<String> = Vec::new();
@@ -158,22 +209,40 @@ fn construct_percentiles<I: PickyIterator<u64>>(hist: &mut HistogramIterator<u64
         if lower_bound as f64 <= v.percentile() &&
            v.count_since_last_iteration() != 0 && 
            v.percentile() <= upper_bound as f64  {
-            let bar = "+".repeat( (v.count_since_last_iteration() as f64 * scaling) as usize);
             out.push(format!("{: >6.2} {: >10} {: >10} {}",
                 v.percentile(),
                 v.value_iterated_to(), 
-                v.count_since_last_iteration(), bar));
+                v.count_since_last_iteration(),
+                bar_string(v.count_since_last_iteration(), max, min, bar_length)));
         }
     } 
     out.reverse();
     out
 }
 
+/// Generate the scaled bar for the bucket represented by `val`
+fn bar_string(val: u64, max: u64, min: u64, max_length: f64) -> String {
+    let scaling = get_scaled(val as f64,
+                                    max as f64,
+                                    min as f64);
+    let bar = "-".repeat( (scaling * max_length) as usize);
+    bar
+}
+
+#[inline]
+fn get_scaled(val: f64, max: f64, min: f64) -> f64 {
+    (val - min) / (max - min)
+}
+
+/// Write the percentile strings to `writer`.
 fn write_percentiles_to<W: Write>(writer: &mut W,
                                  percentiles: &Vec<String>, 
-                                 max_lines: usize)
+                                 max_lines: usize,
+                                 no_info: bool)
 -> Result<(), std::io::Error> {
-    writer.write_all(format!("Percentile  bucket      count\n").as_ref())?;
+    if !no_info {
+        writer.write_all(format!("Percentile  bucket      count\n").as_ref())?;
+    }
     let line_count = if percentiles.len() < max_lines { percentiles.len() } else { max_lines };
     for l in percentiles[0..line_count].iter() {
         writer.write_all(format!("{}\n", l).as_ref())?;
